dffb94ce2729bc6927647d43f5145fc57437d0a1
Zeynel Şahin <zynl.shn@hotmail.com>
2022-05-15 13:57:22 +0300

    Login formu eklendi
    Signed-off-by: Zeynel Şahin <zynl.shn@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {NgModule} from "@angular/core";
 import {ProductComponent} from "./components/product/product.component";
 import {CategoryComponent} from "./components/category/category.component";
 import {ProductAddComponent} from "./components/product-add/product-add.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
   {path:"products",pathMatch:"full",component:ProductComponent},
   {path:"categories",component:CategoryComponent},
   {path:"products/category/:categoryId",component:ProductComponent},
-  {path:"products/add",component:ProductAddComponent}
+  {path:"products/add",component:ProductAddComponent},
+  {path:"login",component:LoginComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {ToastrModule} from "ngx-toastr";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ProductAddComponent } from './components/product-add/product-add.compon
     VatAddedPipe,
     FilterPipePipe,
     CartSummaryComponent,
-    ProductAddComponent
+    ProductAddComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
